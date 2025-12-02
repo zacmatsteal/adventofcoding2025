3d5c88210dcaff744b88797244d4357fc62b199e
zacmatsteal <zacbourgeois@gmail.com>
2025-12-02 06:16:02 -0500

    Day 1 part 2 finished!

--- day1_p2/day1.py
+++ day1_p2/day1.py
@@ -14,21 +14,17 @@ try:
             
             if rotation[0] == "L":
                 dial_pnt -= nb_clicks
+                if old_pnt > 0 and dial_pnt <= 0:
+                    password += 1
             else:
                 dial_pnt += nb_clicks
 
-            if (dial_pnt > MAX_ROTATIONS and old_pnt < MAX_ROTATIONS):
-                password += 1
-                password += dial_pnt // MAX_ROTATIONS - 1
+            if (dial_pnt >= MAX_ROTATIONS):
+                password += dial_pnt // MAX_ROTATIONS
             if (dial_pnt < 0):
-                if old_pnt > 0:
-                    password += 1
                 password += (abs(dial_pnt)) // MAX_ROTATIONS
             
             dial_pnt %= MAX_ROTATIONS
-            
-            if dial_pnt == 0:
-                password += 1
 
     print("The password is: " + str(password))
 
