875ae75e5e4c759a6889ce5dca75e58927812951
zacmatsteal <zacbourgeois@gmail.com>
2025-12-02 04:45:16 -0500

    Bad fix

--- Day_1/day1.py
+++ Day_1/day1.py
@@ -4,20 +4,41 @@ MAX_ROTATIONS = 100
 dial_pnt = 50
 password = 0
 
+print(dial_pnt)
 try:
     with open(ROTATION_PATH) as rotations:
         for rotation in rotations:
             nb_clicks = int(rotation[1:])
+            old_pnt = dial_pnt
             
             if rotation[0] == "L":
                 dial_pnt -= nb_clicks
             else:
                 dial_pnt += nb_clicks
+
+            print("Before: "+str(dial_pnt))
             
-            dial_pnt %= MAX_ROTATIONS
-            
-            if dial_pnt == 0:
+
+            if dial_pnt < 0:
+                while dial_pnt < 0:
+                    password += 1
+                    dial_pnt += MAX_ROTATIONS
+                    print("Add +1")
+                    print("Now: " + str(password))
+                if old_pnt == 0:
+                    password -= 1
+
+            if dial_pnt == 0 and old_pnt != 0:
                 password += 1
+                print("Add +1")
+                print("Now: " + str(password))
+            
+            while dial_pnt >= MAX_ROTATIONS:
+                dial_pnt -= MAX_ROTATIONS
+                print("Add +1")
+                print("Now: " + str(password))
+
+            print("After: "+str(dial_pnt))
 
     print("The password is: " + str(password))
 
