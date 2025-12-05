0171e60bb0cd9383adaa9adbb190823b7ac8c890
zacmatsteal <zacbourgeois@gmail.com>
2025-12-05 00:48:16 -0500

    Day 4 Part 2

---
+++ day4_p2/day4.py
@@ -0,0 +1,113 @@
+INPUT_PATH = "input\\input.txt"
+OUTPUT_PATH = "output\\output.txt"
+
+PAPER_CHAR = '@'
+REMOVE_CHAR = 'x'
+
+def main():
+    matrix = parse_input()
+    solution = solve(matrix)
+
+    print("The solution is: " + str(solution))
+    store_output(solution)
+
+def parse_input():
+    matrix = list()
+
+    try:
+        with open(INPUT_PATH) as input:
+            for line in input:
+                matrix.append(line.rstrip('\n')) # Removes the \n at the end of each line
+
+        line_len = len(matrix[0])
+        for line in matrix:
+            if len(line) != line_len:
+                raise Exception("The length of each line of the map must be the same")
+        return matrix
+    except:
+        raise Exception("Error in the parsing of the input")
+
+def store_output(output):
+    try:
+        with open(OUTPUT_PATH, "w") as file:
+            file.write(str(output))
+    except:
+        raise Exception("Error when trying to store the output")
+
+
+def solve(paperMap: list[str]):
+    solution = -1
+    new_solution = 0
+    while (new_solution != solution):
+        solution = new_solution
+        heatmap = createHeatmap(paperMap)
+
+        result = countPapersAccessible(paperMap, heatmap, solution)
+        new_solution = result[0]
+        paperMap = result[1]
+
+    return solution
+
+def countPapersAccessible(paperMap: list(), heatmap: list(), solution: int):
+    for line in range(len(heatmap)):
+        for col in range(len(heatmap)):
+            if heatmap[line][col] < 4 and paperMap[line][col] == PAPER_CHAR:
+                mapLine = paperMap[line]
+                paperMap[line] = mapLine[:col] + REMOVE_CHAR + mapLine[col + 1:]
+                solution += 1
+    
+    return (solution, paperMap)
+
+def createHeatmap(paperMap: list[str]):
+    heatmap = createZeroMap(len(paperMap), len(paperMap[0]))
+
+    for line in range(len(heatmap)):
+        for col in range(len(heatmap)):
+            if paperMap[line][col] == PAPER_CHAR:
+                heatmap = createPaperImpact(heatmap, line, col)
+
+    return heatmap
+
+def createPaperImpact(heatmap: list(), line: int, col:int):
+    isMaxLeft = col == 0
+    isMaxRight = col == len(heatmap[0]) - 1
+    isMaxTop = line == 0
+    isMaxBottom = line == len(heatmap) - 1
+
+    if (not isMaxLeft):
+        heatmap[line][col-1] += 1
+        
+    if (not isMaxRight):
+        heatmap[line][col+1] += 1
+        
+    if (not isMaxTop):
+        heatmap[line-1][col] += 1
+        if (not isMaxLeft):
+            heatmap[line-1][col-1] += 1
+        if (not isMaxRight):
+            heatmap[line-1][col+1] += 1
+            
+    if (not isMaxBottom):
+        heatmap[line+1][col] += 1
+        if (not isMaxLeft):
+            heatmap[line+1][col-1] += 1
+        if (not isMaxRight):
+            heatmap[line+1][col+1] += 1
+
+    return heatmap
+
+def createZeroMap(lines: int, cols: int):
+    zeroMap = list()
+
+    for _ in range(lines):
+        line = []
+        
+        for _ in range(cols):
+            line.append(0)
+
+        zeroMap.append(line)
+
+    return zeroMap
+
+if __name__=="__main__":
+    main()
